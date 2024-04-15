4e608ef7aa08b9481a5be0994a543f3eabb176dc
Vikram Damani <vikram.damani@damani.ch>
2024-04-15 14:17:45 +0200

    formatting for readability

--- stepper_test/stepper_test.ino
+++ stepper_test/stepper_test.ino
@@ -1,28 +1,14 @@
-///////////////////////////////////////////////
-//         Multi Material DLP Printing       //
-//               Pump Control Code           //
-//                                           //
-//                                           //
-//                  Dimitar                  //
-//                   Boev                    //
-//                                           //
-//                                           //
-//                ETH ZURICH                 //
-//                                           //
-//     ACOUSTIC ROBOTICS SYSTEMS LABORATORY  //
-///////////////////////////////////////////////
-
-
 #define STEPS_PER_REVOLUTION 200
 #define MICROSTEPPING_FACTOR 16
 
 #define CALC_HALF_PULSE_FACTOR(MICROSTEPPING_FACT) (1000000L * 360L / 2L / STEPS_PER_REVOLUTION / MICROSTEPPING_FACT)
 #define HALF_PULSE_FACTOR CALC_HALF_PULSE_FACTOR(MICROSTEPPING_FACTOR)
 
-const float SMALL_DISTANCE = 5; // 5um
-const float LARGE_DISTANCE = 10;  // 10um
+const float SMALL_DISTANCE = 5;  // 5um
+const float LARGE_DISTANCE = 10; // 10um
 
-enum AxisMoveType {
+enum AxisMoveType
+{
     AXIS_MOVE_NONE,
     SMALL_POS,
     SMALL_NEG,
@@ -38,23 +24,21 @@ boolean newData = false;
 // STEP_PIN , DIR_PIN for small CNC Shield
 // X - 2,5 ; Y - 3,6 ; Z - 4,7 ; R - 12,13
 
-
 // Motor pins
 #define STEP_PIN_1 2
-#define DIR_PIN_1  5
+#define DIR_PIN_1 5
 
 #define STEP_PIN_2 3
-#define DIR_PIN_2  6
+#define DIR_PIN_2 6
 
 #define STEP_PIN_3 4
-#define DIR_PIN_3  7
+#define DIR_PIN_3 7
 
 #define STEP_PIN_4 12
-#define DIR_PIN_4  13
+#define DIR_PIN_4 13
 
 #define STEP_PIN_5 34
-#define DIR_PIN_5  36
-
+#define DIR_PIN_5 36
 
 const int NUM_MOTORS = 5;
 const int STEP_PINS[NUM_MOTORS] = {STEP_PIN_1, STEP_PIN_2, STEP_PIN_3, STEP_PIN_4, STEP_PIN_5};
@@ -69,54 +53,67 @@ int stepsToMove[NUM_MOTORS] = {0, 0, 0, 0, 0}; // Add this global variable to st
 
 AxisMoveType AxisMove[NUM_MOTORS] = {AXIS_MOVE_NONE, AXIS_MOVE_NONE, AXIS_MOVE_NONE, AXIS_MOVE_NONE, AXIS_MOVE_NONE};
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     pinMode(STEP_PIN_1, OUTPUT);
     pinMode(DIR_PIN_1, OUTPUT);
-    
+
     pinMode(STEP_PIN_2, OUTPUT);
     pinMode(DIR_PIN_2, OUTPUT);
-    
+
     pinMode(STEP_PIN_3, OUTPUT);
     pinMode(DIR_PIN_3, OUTPUT);
-    
+
     pinMode(STEP_PIN_4, OUTPUT);
     pinMode(DIR_PIN_4, OUTPUT);
-    
+
     pinMode(STEP_PIN_5, OUTPUT);
     pinMode(DIR_PIN_5, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
     receiveMarkers();
     processReceivedData();
-    for (int i = 0; i < NUM_MOTORS; i++) {
+    for (int i = 0; i < NUM_MOTORS; i++)
+    {
         handleContinuousRotation(i);
     }
 }
 
-void receiveMarkers() {
+void receiveMarkers()
+{
     static bool receivingInProgress[20] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
     static byte index = 0;
     char startMarkers[20] = {'<', '[', '(', '{', '@', '-', '$', 'A', 'C', 'E', 'G', 'I', 'K', 'M', 'O', 'Q', 'S', 'U', 'W', 'Y'};
     char endMarkers[20] = {'>', ']', ')', '}', '!', '%', '&', 'B', 'D', 'F', 'H', 'J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z'};
     char data;
 
-    while (Serial.available() > 0 && !newData) {
+    while (Serial.available() > 0 && !newData)
+    {
         data = Serial.read();
 
-        for (int i = 0; i < 20; i++) {
-            if (receivingInProgress[i]) {
-                if (data != endMarkers[i]) {
+        for (int i = 0; i < 20; i++)
+        {
+            if (receivingInProgress[i])
+            {
+                if (data != endMarkers[i])
+                {
                     receivedChars[index++] = data;
-                    if (index >= NUM_CHARS) index = NUM_CHARS - 1;
-                } else {
+                    if (index >= NUM_CHARS)
+                        index = NUM_CHARS - 1;
+                }
+                else
+                {
                     receivedChars[index] = '\0'; // Terminate the string
                     receivingInProgress[i] = false;
                     index = 0;
                     newData = true;
                 }
-            } else if (data == startMarkers[i]) {
+            }
+            else if (data == startMarkers[i])
+            {
                 memset(markerFlags, false, sizeof(markerFlags));
                 markerFlags[i] = true;
                 receivingInProgress[i] = true;
@@ -125,42 +122,59 @@ void receiveMarkers() {
     }
 }
 
-
-void processReceivedData() {
-    if (!newData) return;
+void processReceivedData()
+{
+    if (!newData)
+        return;
     int markerIndex = identifyReceivedMarker();
     int motorIndex = markerIndex / 4;
-//    Serial.println("Identified motor index: ");
-//    Serial.print(motorIndex);
-//    Serial.println(" ");
+    //    Serial.println("Identified motor index: ");
+    //    Serial.print(motorIndex);
+    //    Serial.println(" ");
     int markerType = markerIndex % 4;
-//    Serial.println("Identified marker type: ");
-//    Serial.print(markerType);
-//    Serial.println(" ");
-    
-    if(motorIndex >= 0 && motorIndex < NUM_MOTORS) {
-        switch(markerType) {
-            case 0: handleContinuousRotationMarker(motorIndex); break;
-            case 1: setRotationSpeed(motorIndex); break;
-            case 2: setStepsNumber(motorIndex); break;
-            case 3: handleSteppedRotation(motorIndex); break;
+    //    Serial.println("Identified marker type: ");
+    //    Serial.print(markerType);
+    //    Serial.println(" ");
+
+    if (motorIndex >= 0 && motorIndex < NUM_MOTORS)
+    {
+        switch (markerType)
+        {
+        case 0:
+            handleContinuousRotationMarker(motorIndex);
+            break;
+        case 1:
+            setRotationSpeed(motorIndex);
+            break;
+        case 2:
+            setStepsNumber(motorIndex);
+            break;
+        case 3:
+            handleSteppedRotation(motorIndex);
+            break;
         }
-    } else {
+    }
+    else
+    {
         // Handle error or unexpected marker index
         Serial.println("Error: Unexpected marker index");
     }
-    
+
     newData = false;
 }
 
-int identifyReceivedMarker() {
-    for (int i = 0; i < 20; i++) {
-        if (markerFlags[i]) return i;
+int identifyReceivedMarker()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        if (markerFlags[i])
+            return i;
     }
     return -1; // Marker not found
 }
 
-void setRotationSpeed(int motorIndex) {
+void setRotationSpeed(int motorIndex)
+{
     speedRotation[motorIndex] = atof(receivedChars);
     half_pulse_duration_us[motorIndex] = HALF_PULSE_FACTOR / speedRotation[motorIndex];
     time_of_next_half_pulse[motorIndex] = micros();
@@ -170,74 +184,85 @@ void setRotationSpeed(int motorIndex) {
     Serial.println(speedRotation[motorIndex]);
 }
 
-void setStepsNumber(int motorIndex) {
-//    int motorIndex = identifyReceivedMarker() / 4; // Example to get motorIndex if needed.
-    stepsToMove[motorIndex] = atof(receivedChars); //convert the array to integer
+void setStepsNumber(int motorIndex)
+{
+    //    int motorIndex = identifyReceivedMarker() / 4; // Example to get motorIndex if needed.
+    stepsToMove[motorIndex] = atof(receivedChars); // convert the array to integer
     Serial.print("Motor ");
     Serial.print(motorIndex + 1);
     Serial.print(" steps to execute: ");
     Serial.println(stepsToMove[motorIndex]);
 }
 
-void handleSteppedRotation(int motorIndex) {
-    switch (receivedChars[0]) {
-        case '0': // Assume '0' means stop or do nothing
-            // Implement stopping the motor or do nothing, as per your application
-            Serial.print("Motor ");
-            Serial.print(motorIndex + 1);
-            Serial.println(" not moving.");
-            steppedEnabled[motorIndex] = false;
-            break;
-        case '1': // Move motor forward
-            steppedEnabled[motorIndex] = true;
-            moveMotor(stepsToMove[motorIndex], STEP_PINS[motorIndex], DIR_PINS[motorIndex], LOW, motorIndex, half_pulse_duration_us[motorIndex]);
-            break;
-        case '2': // Move motor backward
-            steppedEnabled[motorIndex] = true;
-            moveMotor(stepsToMove[motorIndex], STEP_PINS[motorIndex], DIR_PINS[motorIndex], HIGH, motorIndex, half_pulse_duration_us[motorIndex]);
-            break;
-        default: // Handle unexpected values
-            Serial.print("Error: Unexpected move direction received for motor ");
-            Serial.println(motorIndex + 1);
-            break;
+void handleSteppedRotation(int motorIndex)
+{
+    switch (receivedChars[0])
+    {
+    case '0': // Assume '0' means stop or do nothing
+        // Implement stopping the motor or do nothing, as per your application
+        Serial.print("Motor ");
+        Serial.print(motorIndex + 1);
+        Serial.println(" not moving.");
+        steppedEnabled[motorIndex] = false;
+        break;
+    case '1': // Move motor forward
+        steppedEnabled[motorIndex] = true;
+        moveMotor(stepsToMove[motorIndex], STEP_PINS[motorIndex], DIR_PINS[motorIndex], LOW, motorIndex, half_pulse_duration_us[motorIndex]);
+        break;
+    case '2': // Move motor backward
+        steppedEnabled[motorIndex] = true;
+        moveMotor(stepsToMove[motorIndex], STEP_PINS[motorIndex], DIR_PINS[motorIndex], HIGH, motorIndex, half_pulse_duration_us[motorIndex]);
+        break;
+    default: // Handle unexpected values
+        Serial.print("Error: Unexpected move direction received for motor ");
+        Serial.println(motorIndex + 1);
+        break;
     }
 }
 
-void handleContinuousRotationMarker(int motorIndex) {
-    switch (receivedChars[0]) {
-        case '0':
-            rotationEnabled[motorIndex] = false;
-            break;
-        case '1':
-            rotationEnabled[motorIndex] = true;
-            digitalWrite(DIR_PINS[motorIndex], LOW);
-            break;
-        case '2':
-            rotationEnabled[motorIndex] = true;
-            digitalWrite(DIR_PINS[motorIndex], HIGH);
-            break;
-        default:
-            Serial.println("Error: Unexpected value for rot");
-            break;
+void handleContinuousRotationMarker(int motorIndex)
+{
+    switch (receivedChars[0])
+    {
+    case '0':
+        rotationEnabled[motorIndex] = false;
+        break;
+    case '1':
+        rotationEnabled[motorIndex] = true;
+        digitalWrite(DIR_PINS[motorIndex], LOW);
+        break;
+    case '2':
+        rotationEnabled[motorIndex] = true;
+        digitalWrite(DIR_PINS[motorIndex], HIGH);
+        break;
+    default:
+        Serial.println("Error: Unexpected value for rot");
+        break;
     }
 }
 
-void handleContinuousRotation(int motorIndex) {
-    if (!rotationEnabled[motorIndex]) return;
-    if (micros() >= time_of_next_half_pulse[motorIndex]) {
+void handleContinuousRotation(int motorIndex)
+{
+    if (!rotationEnabled[motorIndex])
+        return;
+    if (micros() >= time_of_next_half_pulse[motorIndex])
+    {
         digitalWrite(STEP_PINS[motorIndex], !digitalRead(STEP_PINS[motorIndex]));
         time_of_next_half_pulse[motorIndex] = micros() + half_pulse_duration_us[motorIndex];
     }
 }
 
-void moveMotor(int steps, int stepPin, int dirPin, int direction, int motorIndex, unsigned long halfPulseDuration) {
+void moveMotor(int steps, int stepPin, int dirPin, int direction, int motorIndex, unsigned long halfPulseDuration)
+{
     digitalWrite(dirPin, direction); // Set the direction of the motor movement
-    for(int i = 0; i < steps; i++) {
-      while (steppedEnabled[motorIndex]) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(halfPulseDuration);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(halfPulseDuration);
-      }
+    for (int i = 0; i < steps; i++)
+    {
+        while (steppedEnabled[motorIndex])
+        {
+            digitalWrite(stepPin, HIGH);
+            delayMicroseconds(halfPulseDuration);
+            digitalWrite(stepPin, LOW);
+            delayMicroseconds(halfPulseDuration);
+        }
     }
 }
